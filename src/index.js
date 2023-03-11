b97394c808c344da43320c9364d94ddcad7cc5f5
REDT <87268805+RafaelTorres0-0@users.noreply.github.com>
2023-03-11 17:59:17 -0400

    nav and css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
+import "./style.css";
 
-function component() {
-    const element = document.createElement('div');
+const navbar = () =>{
+  const nav = document.createElement('nav');
+  nav.classList.add("sidenav")
   
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const ul = document.createElement('ul');
+  ul.classList.add("menu");
+  //class
+  const liT = document.createElement('li');
+  //class
+  //render
+  liT.innerText = 'Todos';
   
-    return element;
-  }
+
+  const liP = document.createElement('li');
+  //class
+  //render  
+  liP.innerHTML = 'Projects';
+  //Sub menu Projects****
+  const subul2 = document.createElement('ul');
+  subul2.classList.add("sub-menu");
+  const subli21 = document.createElement('li');
+  subli21.textContent = "Project1";
+  const subli22 = document.createElement('li');
+  subli22.textContent = "Project1";
+  subul2.appendChild(subli21);
+  subul2.appendChild(subli22);
+
+
   
-  document.body.appendChild(component());
+
+  ul.appendChild(liT);
+  ul.appendChild(subul1);
+  ul.appendChild(liP);
+  ul.appendChild(subul2);
+  nav.appendChild(ul);
+
+return nav;
+}
+
+const main = () => {
+
+  return 0;
+}
+
+
+  document.body.appendChild(navbar());
+  document.body.appendChild(main());
