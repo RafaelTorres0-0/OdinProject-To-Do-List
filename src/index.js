4c0cc7eb8ac524534dd9b1368d88b01699fa5465
REDT <87268805+RafaelTorres0-0@users.noreply.github.com>
2023-03-11 22:06:47 -0400

    starting the main document

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 import "./style.css";
 
-const navbar = () =>{
+const navbar = () => {
   const nav = document.createElement('nav');
   nav.classList.add("sidenav")
-  
+
   const ul = document.createElement('ul');
   ul.classList.add("menu");
+  
   //class
   const liT = document.createElement('li');
-  //class
-  //render
-  liT.innerText = 'Todos';
+  liT.innerText = '//To-Do';
+  const todoul = document.createElement('ul');
+  todoul.classList.add("sub-menu");
+  //Home
+  const home = document.createElement('li');
+  home.textContent = "Home";
+  //render ALL
+  todoul.appendChild(home);
+  const today = document.createElement('li');
+  today.textContent = "Today";
+  //render Today
+  todoul.appendChild(today);
+  const week = document.createElement('li');
+  week.textContent = "Week";
+  //render week
+  todoul.appendChild(week);
   
+
+
 
   const liP = document.createElement('li');
   //class
   //render  
-  liP.innerHTML = 'Projects';
+  liP.innerHTML = '//Projects';
   //Sub menu Projects****
   const subul2 = document.createElement('ul');
   subul2.classList.add("sub-menu");
@@ -26,24 +42,39 @@ const navbar = () =>{
   subli22.textContent = "Project1";
   subul2.appendChild(subli21);
   subul2.appendChild(subli22);
+  const addButton = document.createElement('button');
+  addButton.classList.add('btn');
+  addButton.textContent = "+";
+  subul2.appendChild(addButton);
 
-
-  
 
   ul.appendChild(liT);
-  ul.appendChild(subul1);
+  ul.appendChild(todoul);
   ul.appendChild(liP);
   ul.appendChild(subul2);
   nav.appendChild(ul);
 
-return nav;
+  return nav;
 }
 
-const main = () => {
+const main = () =>{
+  const main = document.createElement('main');
+  main.classList.add('main');
 
-  return 0;
+  const header = document.createElement('header');
+  header.classList.add('header');
+
+  const title = document.createElement('h2');
+  title.textContent = "To-Do";
+  header.appendChild(title);
+
+  const date = document.createElement('h2');
+  date.textContent = "Date";
+  header.appendChild(date);
+
+  main.appendChild(header);
+  return main;
 }
 
-
-  document.body.appendChild(navbar());
-  document.body.appendChild(main());
+document.body.appendChild(navbar());
+document.body.appendChild(main());
